43290038b447213ebee144dd63a8641f337c93e9
Duc <ngohoaiduc08@gmail.com>
2024-08-13 09:16:49 +0700

    update admin layouts

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ app.set("view engine", "pug");
 // Routes
 const route = require("./routes/client/index.route");
 route(app);
-
+// Route Admin
+const routeAdmin = require("./routes/admin/index.route");
+routeAdmin(app);
+// App Local Variables
+const systemConfig = require("./config/system");
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
