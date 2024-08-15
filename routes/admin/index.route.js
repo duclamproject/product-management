109b2e0e5770ab0e1f72e1fcf49716756e84768c
Duc <ngohoaiduc08@gmail.com>
2024-08-15 21:49:55 +0700

    Dựng khung admin-products

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,8 @@
 const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
 const systemConfig = require("../../config/system");
 module.exports = (app) => {
-  app.use(systemConfig.prefixAdmin + "/dashboard", dashboardRoutes);
+  const PATH_ADMIN = systemConfig.prefixAdmin;
+  app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
+  app.use(PATH_ADMIN + "/products", productRoutes);
 };
