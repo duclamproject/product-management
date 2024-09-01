90c4efe585385ae12c31ae6bd2e50c55a7f4a7f7
Duc <ngohoaiduc08@gmail.com>
2024-09-01 12:33:15 +0700

    pagination update

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,11 +2,10 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 module.exports.index = async (req, res) => {
   // Bộ lọc
   const filterStatus = filterStatusHelper(req.query);
-  // console.log(filterStatus);
-
   let find = {
     deleted: false,
   };
@@ -16,18 +15,33 @@ module.exports.index = async (req, res) => {
 
   // Keyword
   const objectSearch = searchHelper(req.query);
-  // console.log(objectSearch);
-
   if (objectSearch.regex) {
     find.title = objectSearch.regex;
   }
-  const products = await Product.find(find);
-  // console.log(products);
+
+  // Pagination
+  // Công thức phân trang (Skip)= (CurrentPage - 1) * LimtIems;
+
+  const countProducts = await Product.countDocuments(find);
+  const objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 4,
+    },
+    req.query,
+    countProducts
+  );
+  // End Pagination
+
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Trang sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
+    pagination: objectPagination,
   });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -35,3 +35,23 @@ if (formSearch) {
   });
 }
 //End Form Search
+
+// Paginations
+const buttonPagination = document.querySelectorAll("[button-pagination]");
+if ((buttonPagination.length = !0)) {
+  let url = new URL(window.location.href);
+
+  buttonPagination.forEach((button) => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+      if (page) {
+        url.searchParams.set("page", page);
+      } else {
+        url.searchParams.delete("page");
+      }
+      window.location.href = url.href;
+    });
+  });
+}
+
+// End Paginations
