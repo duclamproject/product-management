46cf61e292213af7446543341f114b9c8f792b5f
Duc <ngohoaiduc08@gmail.com>
2024-09-28 21:26:53 +0700

    Update stt

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,8 +1,11 @@
+let count = 0;
 function createTree(arr, parentID = "") {
   const tree = [];
   arr.forEach((item) => {
     if (item.parent_id === parentID) {
       const newBranch = item;
+      count++;
+      newBranch.index = count;
       const children = createTree(arr, item.id);
       if (children.length > 0) {
         newBranch.children = children;
@@ -13,6 +16,7 @@ function createTree(arr, parentID = "") {
   return tree;
 }
 module.exports.tree = (arr, parentID) => {
+  count = 0;
   const tree = createTree(arr, (parentID = ""));
   return tree;
 };
