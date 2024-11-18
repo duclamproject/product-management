ee0ce121e53abbe46fef9eb2e6a7b306c821732f
Duc <ngohoaiduc08@gmail.com>
2024-11-18 15:18:15 +0700

    Sub-menu client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,4 @@
 // [GET] /products
-
 const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
   const products = await Product.find({
@@ -13,7 +12,6 @@ module.exports.index = async (req, res) => {
     ).toFixed(2);
     return item;
   });
-  console.log(products);
 
   res.render("client/pages/products/index", {
     pageTitle: "Trang danh sách sản phẩm",
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,8 @@
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
-
+const middleware = require("../../middlewares/client/category.middleware");
 module.exports = (app) => {
+  app.use(middleware.category);
   app.use("/", homeRoutes);
   app.use("/products", productRoutes);
 };
