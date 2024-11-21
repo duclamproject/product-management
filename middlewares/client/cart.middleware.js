313500eed26167fed9c693448055a569f476cea3
Duc <ngohoaiduc08@gmail.com>
2024-11-21 21:23:12 +0700

    mini cart - header.pug/middleware

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -5,12 +5,21 @@ module.exports.cart = async (req, res, next) => {
     const cart = new Cart();
     await cart.save();
     console.log(cart);
-    const expiresTime = 31536000;
+    const expiresTime = 365 * 24 * 60 * 60 * 1000;
     res.cookie("cartId", cart.id, {
       expires: new Date(Date.now() + expiresTime),
+      httpOnly: true,
     });
   } else {
     // Khi đã đăng nhập
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId,
+    });
+    cart.totalQuantity = cart.products.reduce(
+      (sum, item) => sum + item.quantity,
+      0
+    );
+    res.locals.miniCart = cart;
   }
   next();
 };
