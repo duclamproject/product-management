5cc26e38dde1f2127ba5f6f3ac4a499bd7a778b5
Duc <ngohoaiduc08@gmail.com>
2024-11-23 01:12:40 +0700

    checkout - order

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,5 +1,6 @@
 const Cart = require("../../models/cart.model");
 const Product = require("../../models/product.model");
+const Order = require("../../models/orderd.model");
 const productHelper = require("../../helpers/products");
 // [GET] :/checkout
 module.exports.index = async (req, res) => {
@@ -21,8 +22,42 @@ module.exports.index = async (req, res) => {
     (sum, item) => sum + item.totalPrice,
     0
   );
+
   res.render("client/pages/checkout/index.pug", {
     pageTitle: "Đặt hàng",
     cartDetail: cart,
   });
+};
+
+module.exports.order = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const userInfor = req.body;
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+  let products = [];
+  for (const product of cart.products) {
+    const objectProduct = {
+      product_id: product.product_id,
+      price: 0,
+      discountPercentage: 0,
+      quantity: product.quantity,
+    };
+    const productInfor = await Product.findOne({
+      _id: product.product_id,
+    });
+    objectProduct.price = productInfor.price;
+    objectProduct.discountPercentage = productInfor.discountPercentage;
+    products.push(objectProduct);
+  }
+  const objectOrder = {
+    cart_id: cartId,
+    userInfor: userInfor,
+    products: products,
+  };
+  const order = new Order(objectOrder);
+  await order.save();
+  await Cart.updateOne({ _id: cartId }, { products: [] });
+  req.flash("success", "Thanh toán thành công!");
+  res.redirect(`/checkout/success/${order.id}`);
 };
--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -2,4 +2,5 @@ const express = require("express");
 const controller = require("../../controllers/client/checkout.controller");
 const route = express.Router();
 route.get("/", controller.index);
+route.post("/order", controller.order);
 module.exports = route;
