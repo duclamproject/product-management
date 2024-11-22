03e7402601d2e737eae19aa4d2ab8437c28d6151
Duc <ngohoaiduc08@gmail.com>
2024-11-22 16:44:23 +0700

    view cart - table products in cart

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,6 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+const productHelper = require("../../helpers/products");
 // [POST] : cart/add/:productId
 module.exports.addPost = async (req, res) => {
   const cartId = req.cookies.cartId;
@@ -37,4 +39,32 @@ module.exports.addPost = async (req, res) => {
 
   req.flash("success", "Thêm sản phẩm vào giỏ hàng thành công!");
   res.redirect("back");
+};
+
+// [GET] :/cart/
+module.exports.index = async (req, res) => {
+  const cart = await Cart.findOne({
+    _id: req.cookies.cartId,
+  });
+  // let totalPrice = 0;
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productId = item.product_id;
+      const productInfor = await Product.findOne({
+        _id: productId,
+      });
+      productInfor.priceNew = productHelper.priceNewProduct(productInfor);
+      item.productInfor = productInfor;
+      item.totalPrice = item.productInfor.priceNew * item.quantity;
+      // totalPrice += item.quantity * productHelper.priceNewProduct(productInfor);
+    }
+  }
+  cart.totalPrice = cart.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
+  res.render("client/pages/cart/index.pug", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart,
+  });
 };
