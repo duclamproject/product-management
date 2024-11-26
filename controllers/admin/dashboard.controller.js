08e7dbdc9bb91be318c28657f1f6bd35e7d1f486
Duc <ngohoaiduc08@gmail.com>
2024-11-26 16:53:15 +0700

    admin - dashboard page

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,7 +1,65 @@
 // [GET] /admin/dashboard
-
-module.exports.dashboard = (req, res) => {
+const ProductCategory = require("../../models/product-category.model");
+const Product = require("../../models/product.model");
+const Account = require("../../models/account.model");
+const User = require("../../models/user.model");
+module.exports.dashboard = async (req, res) => {
+  const statistic = {
+    categoryProduct: {
+      total: await ProductCategory.countDocuments({
+        deleted: false,
+      }),
+      active: await ProductCategory.countDocuments({
+        status: "active",
+        deleted: false,
+      }),
+      inactive: await ProductCategory.countDocuments({
+        status: "inactive",
+        deleted: false,
+      }),
+    },
+    product: {
+      total: await Product.countDocuments({
+        deleted: false,
+      }),
+      active: await Product.countDocuments({
+        status: "active",
+        deleted: false,
+      }),
+      inactive: await Product.countDocuments({
+        status: "inactive",
+        deleted: false,
+      }),
+    },
+    account: {
+      total: await Account.countDocuments({
+        deleted: false,
+      }),
+      active: await Account.countDocuments({
+        status: "active",
+        deleted: false,
+      }),
+      inactive: await Account.countDocuments({
+        status: "inactive",
+        deleted: false,
+      }),
+    },
+    user: {
+      total: await User.countDocuments({
+        deleted: false,
+      }),
+      active: await User.countDocuments({
+        status: "active",
+        deleted: false,
+      }),
+      inactive: await User.countDocuments({
+        status: "inactive",
+        deleted: false,
+      }),
+    },
+  };
   res.render("admin/pages/dashboard/index", {
     pageTitle: "Trang tổng quan",
+    statistic: statistic,
   });
 };
