d643dff3730f59746a1361094aa62a125ca09abc
Duc <ngohoaiduc08@gmail.com>
2024-11-27 17:39:04 +0700

    install socket.io

--- index.js
+++ index.js
@@ -6,49 +6,73 @@ const path = require("path");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const moment = require("moment");
+
 // .env
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
-// Mehod Override
-app.use(methodOverride("_method"));
-// Body-Parser
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// mongoose - database check
-const database = require("./config/database");
-database.connect();
+
+// Define: Socket http
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
 // public folder
 app.use(express.static(`${__dirname}/public`));
+
 // PugJS
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
+
+// Socket IO
+const io = new Server(server);
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
+});
+
+// Cut
+// Mehod Override
+app.use(methodOverride("_method"));
+
+// Body-Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// mongoose - database check
+const database = require("./config/database");
+database.connect();
+// End cut
+
 // Express Flash
 app.use(cookieParser("JKJKJKFHJK"));
 app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
+
 // TinyMCE
 app.use(
   "/tinymce",
   express.static(path.join(__dirname, "node_modules", "tinymce"))
 );
+
 // Routes
 const routeClient = require("./routes/client/index.route");
 routeClient(app);
+
 // Route Admin
 const routeAdmin = require("./routes/admin/index.route");
 routeAdmin(app);
+
 // App Local Variables
 const systemConfig = require("./config/system");
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 app.locals.moment = moment;
+
 // 404 Not Found
 app.get("*", (req, res) => {
   res.render("client/pages/error/404.pug", {
     pageTitle: "404 Not Found",
   });
 });
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
