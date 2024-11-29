d55f606592ac5044fbe7764a717fb756581e4540
Duc <ngohoaiduc08@gmail.com>
2024-11-29 22:41:22 +0700

    chat - normal

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,10 +1,33 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
+
   // SocketIo
-  _io.on("connection", (socket) => {
-    console.log("A user connected: ", socket.id);
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      // Lưu vào DB
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+      await chat.save();
+    });
   });
   // End SocketIo
+  // Get data
+  const chats = await Chat.find({
+    deleted: false,
+  });
+  console.log(chats);
+  for (const chat of chats) {
+    const inforUser = await User.findOne({
+      _id: chat.user_id,
+    }).select("fullName");
+    chat.inforUser = inforUser;
+  }
   res.render("client/pages/chat/index.pug", {
     pageTitle: "Chat",
+    chats: chats,
   });
 };
