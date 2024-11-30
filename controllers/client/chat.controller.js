82c4dd0610bc6718a08884fcc3c236861b4aa6ce
Duc <ngohoaiduc08@gmail.com>
2024-11-30 15:48:04 +0700

    chat realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,7 +1,9 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
+
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
 
   // SocketIo
   _io.once("connection", (socket) => {
@@ -12,6 +14,13 @@ module.exports.index = async (req, res) => {
         content: content,
       });
       await chat.save();
+      // Trả data cho client
+      _io.emit("SERVER_RETURN_MESSAGE", {
+        // Trả userId là cái user gửi message lên cho server
+        userId: userId,
+        fullName: fullName,
+        content: content,
+      });
     });
   });
   // End SocketIo
@@ -19,7 +28,7 @@ module.exports.index = async (req, res) => {
   const chats = await Chat.find({
     deleted: false,
   });
-  console.log(chats);
+  // console.log(chats);
   for (const chat of chats) {
     const inforUser = await User.findOne({
       _id: chat.user_id,
--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,23 @@ if (formSendData) {
   });
 }
 // END CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const body = document.querySelector(".chat .inner-body");
+  const div = document.createElement("div");
+  const userId = document.querySelector("[my-id]").getAttribute("my-id");
+  let htmlFullName = "";
+  if (userId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  }
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
+  body.appendChild(div);
+});
+// End SERVER_RETURN_MESSAGE
