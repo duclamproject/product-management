0afa87ae446a3ab4e20250e0c203be86cd27b1b7
Duc <ngohoaiduc08@gmail.com>
2025-03-29 21:00:57 +0700

    show list users

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,3 +1,9 @@
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
+const userMiddleware = require("../../middlewares/client/user.middleware");
+const settingMiddleware = require("../../middlewares/client/setting.middleware");
+const authMiddleware = require("../../middlewares/client/auth.middleware");
+
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
@@ -5,10 +11,8 @@ const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
 const chatRoutes = require("./chat.route");
-const categoryMiddleware = require("../../middlewares/client/category.middleware");
-const cartMiddleware = require("../../middlewares/client/cart.middleware");
-const userMiddleware = require("../../middlewares/client/user.middleware");
-const settingMiddleware = require("../../middlewares/client/setting.middleware");
+const usersRoutes = require("./users.route");
+
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cart);
@@ -22,5 +26,6 @@ module.exports = (app) => {
   app.use("/cart/", cartRoutes);
   app.use("/checkout/", checkoutRoutes);
   app.use("/user/", userRoutes);
-  app.use("/chat", chatRoutes);
+  app.use("/chat", authMiddleware.requireAuth, chatRoutes);
+  app.use("/users/", authMiddleware.requireAuth, usersRoutes);
 };
